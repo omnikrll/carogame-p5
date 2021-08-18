97ab6b7cba9b8ddf0a2c0f13617b7be1d1d560a9
Karl Hohn <kfhohn@gmail.com>
2021-08-17 20:54:00 -0400

    include fonts via css, change rectangles to buttons

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,7 @@
-let data,
+let gotham,
+	ds_digital,
+	digital7,
+	data,
 	post,
 	displayPost,
 	button1,
@@ -14,32 +17,53 @@ let scoreboard = {
 let keyCodes = [70, 74, 32];
 
 function preload() {
+	gotham = loadFont('assets/fonts/gotham/Gotham/Gotham-Book.otf');
+	digital7 = loadFont('assets/fonts/digital_7/digital-7 (mono italic).ttf');
+	ds_digital = loadFont('assets/fonts/ds_digital/DS-DIGIT.TTF');
 	data = loadJSON('/js/data.json');
 }
 
 function setup() {
 	createCanvas(1024, 768);
 	console.log(data.content);
-	noLoop();
 }
 
 function draw() {
 	renderPlayfield();
-	renderPost();
+
+	console.log(post);
+	if (post == undefined) {
+		console.log(post == undefined);
+		renderPost();
+		loop();
+	}
 }
 
 function renderPlayfield() {
 	background('#959595');
 	
 	displayPost = rect(192, 80, 640, 240);
-	button1 = rect(192, 480, 180, 80);
-	text('Approve', 200, 488, 172, 72);
 
-	button2 = rect(422, 480, 180, 80);
-	text('AI Decides', 430, 488, 172, 72);
+	button1 = createButton('Approve');
+	button1.size(180, 80);
+	button1.position(192, 480);
+	button1.style('font-family', 'Gotham');
+	button1.style('font-weight', 'lighter');
+	button1.style('text-align', 'center');
 
-	button3 = rect(652, 480, 180, 80);	
-	text('Harmful', 660, 488, 172, 72);
+	button2 = createButton('AI Decides');
+	button2.size(180, 80);
+	button2.position(422, 480);
+	button2.style('font-family', 'Gotham');
+	button2.style('font-weight', 'lighter');
+	button2.style('text-align', 'center');
+
+	button3 = createButton('Harmful');
+	button3.size(180, 80);
+	button3.position(652, 480);	
+	button3.style('font-family', 'Gotham');
+	button3.style('font-weight', 'lighter');
+	button3.style('text-align', 'center');
 }
 
 function getRandomPost() {
@@ -49,9 +73,11 @@ function getRandomPost() {
 }
 
 function renderPost() {
+	noLoop();
 	post = getRandomPost();
 	textSize(24);
 	text(post.text, 216, 104, 616, 216);
+	textFont(gotham);
 }
 
 function approve() {
@@ -74,24 +100,24 @@ function pass() {
 	scoreboard.pass++;
 }
 
-function keyPressed(event) {
-	if (keyCodes.indexOf(keyCode) > -1) {
-		event.preventDefault();	
-		console.log(scoreboard);
-		switch (keyCode) {
-			case 70:
-				approve();
-				break;
-			case 74:
-				deny();
-				break;
-			case 32:
-				pass();
-				break;
+// function keyPressed(event) {
+// 	if (keyCodes.indexOf(keyCode) > -1) {
+// 		event.preventDefault();	
+// 		console.log(scoreboard);
+// 		switch (keyCode) {
+// 			case 70:
+// 				approve();
+// 				break;
+// 			case 74:
+// 				deny();
+// 				break;
+// 			case 32:
+// 				pass();
+// 				break;
 
-		}
-		renderPlayfield();
-		renderPost();
-	}
+// 		}
+// 		renderPlayfield();
+// 		renderPost();
+// 	}
 
-}
+// }
