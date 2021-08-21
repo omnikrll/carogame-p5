2c0e31dd8ed1931d193bbbc11dc77079ab41ada0
Karl Hohn <kfhohn@gmail.com>
2021-08-21 01:25:27 -0400

    moved a bunch of styles to css. timeout works

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,18 @@
-let gotham,
-	ds_digital,
-	digital7,
-	bg,
-	div,
-	data,
-	post,
-	displayPost,
-	button1,
-	button2,
-	button3;
+let gotham = false,
+	ds_digital = false,
+	digital7 = false,
+	bg = false,
+	mainWindow = false,
+	data = false,
+	post = false,
+	postDiv = false,
+	displayPost = false,
+	button1 = false,
+	button2 = false,
+	button3 = false,
+	menu = false,
+	timerDisplay = false,
+	timerFontSize = 32;
 
 let scoreboard = {
 	"correct": 0,
@@ -16,7 +20,19 @@ let scoreboard = {
 	"pass": [0,0,0,0,0,0,0,0,0]
 }
 
-let keyCodes = [70, 74, 32];
+let timer = 10;
+
+let run = false;
+
+let reasons = [
+	'Sexism / Misogyny',
+	'Racism / Xenophobia',
+	'Ethnic or Religious Slur',
+	'Physical Threats',
+	'Sexual Threats',
+	'Other',
+	'Harassment (negative)'
+];
 
 function preload() {
 	gotham = loadFont('assets/fonts/gotham/Gotham/Gotham-Book.otf');
@@ -27,56 +43,87 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(displayWidth, displayHeight);
-	noLoop();
+	let canvas = createCanvas(displayWidth, displayHeight);
+	canvas.parent('sketch');
+	// noLoop();
 }
 
 function draw() {
-	renderPlayfield();
-	if (post == undefined) {
+	if (!post) {
+		renderPlayfield();
 		renderPost();
-	} else {
-		loop();
+	}
+
+
+	if (run && frameCount % 60 == 0 && timer > 0) {
+		timer--;
+
+		if (timer <= 5) {
+			timerFontSize += 4;
+			timerDisplay.style('font-size', timerFontSize + 'px');
+		}
+	}
+
+	timerDisplay.html('00:' + timer.toString().padStart(2,'0')); 
+
+	if (timer <= 10) {
+		timerDisplay.style('color', 'red');
+	}
+
+
+
+	if (timer == 0) {
+		roundOver();
 	}
 }
 
 function renderPlayfield() {
-	background(bg);
+	// background(bg);
 
-	div = createDiv().size(640, 480);
-	div.style('background', '#959595');
-	div.style('border', '3px outset black');
-	div.style('border-radius', '5px');
-	div.center();
+	if (!mainWindow) {
+		mainWindow = createDiv().addClass('mainWindow');
+		mainWindow.parent('sketch');
+	}
+	// div.center();
+
+	if (!timerDisplay) {
+		timerDisplay = createDiv().addClass('timerDisplay');
+		timerDisplay.parent(mainWindow);
+		timerDisplay.style('font-size', timerFontSize + 'px');
+	}
 	
-	// displayPost = rect(192, 80, 640, 240);
+	if (!button1) {
+		button1 = createButton('Approve');
+		button1.parent(mainWindow);
+		button1.size(180, 80);
+		button1.position(15, 367);
+		button1.style('font-family', 'Gotham');
+		button1.style('font-weight', 'lighter');
+		button1.style('text-align', 'center');
+		button1.mousePressed(approve);
+	}
 
-	button1 = createButton('Approve');
-	button1.parent(div);
-	button1.size(180, 80);
-	button1.position(15, 367);
-	button1.style('font-family', 'Gotham');
-	button1.style('font-weight', 'lighter');
-	button1.style('text-align', 'center');
-	button1.mousePressed(approve);
+	if (!button2) {
+		button2 = createButton('AI Decides');
+		button2.parent(mainWindow);
+		button2.size(180, 80);
+		button2.position(228, 367);
+		button2.style('font-family', 'Gotham');
+		button2.style('font-weight', 'lighter');
+		button2.style('text-align', 'center');
+		button2.mousePressed(pass);
+	}
 
-	button2 = createButton('AI Decides');
-	button2.parent(div);
-	button2.size(180, 80);
-	button2.position(228, 367);
-	button2.style('font-family', 'Gotham');
-	button2.style('font-weight', 'lighter');
-	button2.style('text-align', 'center');
-	button2.mousePressed(pass);
-
-	button3 = createButton('Harmful');
-	button3.parent(div);
-	button3.size(180, 80);
-	button3.position(441, 367);	
-	button3.style('font-family', 'Gotham');
-	button3.style('font-weight', 'lighter');
-	button3.style('text-align', 'center');
-	button3.mousePressed(denyMenu);
+	if (!button3) {
+		button3 = createButton('Harmful');
+		button3.parent(mainWindow);
+		button3.size(180, 80);
+		button3.position(441, 367);	
+		button3.style('font-family', 'Gotham');
+		button3.style('font-weight', 'lighter');
+		button3.style('text-align', 'center');
+		button3.mousePressed(denyMenu);
+	}
 }
 
 function getRandomPost() {
@@ -88,15 +135,18 @@ function getRandomPost() {
 function renderPost() {
 	noLoop();
 	post = getRandomPost();
-	let postDiv = createDiv(post.text).size(606, 240);
-	postDiv.parent(div);
+	if (typeof postDiv == 'object') postDiv.remove();
+	postDiv = createDiv(post.text).size(606, 240);
+	postDiv.parent(mainWindow);
 	postDiv.position(15, 15);
 	postDiv.style('font-family', 'Gotham');
 	postDiv.style('font-size', '18px');
 	postDiv.style('border', '1px solid black');
 	postDiv.style('padding', '2em');
 	postDiv.style('box-sizing', 'border-box');
+	loop();
 }
+
 
 function approve() {
 	if (post.human_rating == 0) {
@@ -108,51 +158,34 @@ function approve() {
 }
 
 function denyMenu() {
-	let menu = createDiv().size(144,320).position(223,300);
+	menu = createDiv().addClass('menu').parent(mainWindow);
 
 	let ul = createElement('ul').parent(menu);
-	createElement('li', 'Sexism / Misogyny').value(1).parent(ul).mousePressed(deny);
-	createElement('li', 'Racism / Xenophobia').value(2).parent(ul).mousePressed(deny);
-	createElement('li', 'Homophobia / Transphobia').value(3).parent(ul).mousePressed(deny);
-	createElement('li', 'Ethnic or Religious Slur').value(4).parent(ul).mousePressed(deny);
-	createElement('li', 'Physical Threats').value(5).parent(ul).mousePressed(deny);
-	createElement('li', 'Sexual Threats').value(6).parent(ul).mousePressed(deny);
-	createElement('li', 'Other').value(7).parent(ul).mousePressed(deny);
-	createElement('li', 'Harassment (negative)').value(8).parent(ul).mousePressed(deny);	
+
+	for (var i=0, ii=reasons.length; i<ii; i++) {
+		let reason = reasons[i];
+		let value = i + 1;
+		let option = createElement('li', reason).parent(ul);	
+		option.mousePressed(function() {
+			deny(value);
+		});
+	}
 }
 
-function deny(event) {
-	console.log(event);
-	// if (post.human_rating > 0) {
-	// 	scoreboard.correct++;
-	// } else {
-	// 	scoreboard.fail++;
-	// }
-	// renderPost();
+function deny(value) {
+	if (!!menu) menu.remove();
+	if (post.human_rating == value) {
+		scoreboard.correct++;
+	} else {
+		scoreboard.fail++;
+	}
+	renderPost();
 }
 
 function pass() {
 	scoreboard.pass++;
 }
 
-// function keyPressed(event) {
-// 	if (keyCodes.indexOf(keyCode) > -1) {
-// 		event.preventDefault();	
-// 		console.log(scoreboard);
-// 		switch (keyCode) {
-// 			case 70:
-// 				approve();
-// 				break;
-// 			case 74:
-// 				deny();
-// 				break;
-// 			case 32:
-// 				pass();
-// 				break;
-
-// 		}
-// 		renderPlayfield();
-// 		renderPost();
-// 	}
-
-// }
+function roundOver() {
+	alert('round over');
+}
